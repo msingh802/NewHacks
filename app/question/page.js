ea68435aab8e42c73a4fb44d79ba0b440ba3bbb3
Raymond2193 <raymond.li2193@gmail.com>
2024-10-27 01:22:30 -0400

    initial commit

--- app/question/page.js
+++ app/question/page.js
@@ -1,7 +1,7 @@
 "use client"  
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const quizQuestions = [
@@ -75,19 +75,51 @@ const quizQuestions = [
 
 
 
-export default function Home() {
+export default function Questions() {
+  const [idxCurrentQuestion, setIdxCurrentQuestion] = useState(0);
+  const [msg, setMsg] = useState("");
+  const [msg2, setMsg2] = useState("");
+  const router = useRouter();
+
+  function compare(value){
+    console.log(value);
+    if(value === quizQuestions[idxCurrentQuestion].answer){
+      setMsg("Correct!");
+      setMsg2("Next Question");
+    }
+    else {
+      setMsg("Incorrect!");
+    }
+
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-6xl font-bold text-center">Welcome to the Emergency Preparedness Quiz</h1>
-      <p className="text-xl mt-4">Test your knowledge on emergency preparedness and disaster management</p>
-      <div className="mt-4">
-        <p className="text-xl">Click below to start the quiz</p>
+    <div className="flex items-center flex-col justify-center h-screen">
+      <p>Question:</p>
+      <p>{quizQuestions[idxCurrentQuestion].question}</p>
+      <p>Options:</p>
+      <div className="flex flex-col items-start gap-4">
+
+        {quizQuestions[idxCurrentQuestion].options.map((option, idx) => {
+          return(
+          <Button key={idx} onClick={(e) => compare(e.target.value)} value={option}>{option}</Button>
+        )
+        })}
       </div>
-      <div className="mt-4">
-        <Link href="/question">
-          <Button>Start Quiz</Button>
-        </Link>
-      </div>
+      <p>{msg}</p>
+      {msg2 && <Button onClick={() => { 
+        setIdxCurrentQuestion(idxCurrentQuestion + 1); 
+        setMsg2("");
+        setMsg("");
+      }}>{msg2}</Button>}
+      
+      <Button onClick={() => setIdxCurrentQuestion(0)}>Restart</Button>
+      <Button onClick={() => {
+        if(idxCurrentQuestion > 0) {
+            setIdxCurrentQuestion((idxCurrentQuestion - 1))
+        }
+        router.push("/")
+      }}>Back</Button>
     </div>
-  )
+  );
 }
